6fe05e09ddc922d8c8f826c1a9451dd8ba88dde0
harryg <harry@gmail.com>
2023-03-19 00:26:13 +0000

    Adjusted weighting to be more logical however more questions may be needed in future to reduce dominance of one of the three user inputs

--- src/components/Pages/Directory/cocktailReccomender.js
+++ src/components/Pages/Directory/cocktailReccomender.js
@@ -1,30 +1,39 @@
 import cocktailsData from './cocktails.json';
 
+// This is how the weighting is calculated, so here the highest number (here it is mood)
+// is the greatest weight. 
+// (This may need to be adjusted more because whichever is the most weight at the moment is very dominant)
 export const weights = {
-  fruit: 1,
-  mood: 2,
-  astrologicalSign: 3,
-};
+    fruit: 2,
+    mood: 3,
+    astrologicalSign: 1,
+  };  
 
-export const recommendCocktail = (fruit, mood, astrologicalSign) => {
+  export const recommendCocktail = (fruit, mood, astrologicalSign) => {
     let maxScore = -1;
     let recommendedCocktail = null;
   
+    // this iterates over each cocktail in the data
     cocktailsData.cocktails.forEach((cocktail) => {
       let score = 0;
   
+      // adds  appropriate weight to  score if  cocktail matches  selected fruit
       if (cocktail.fruit === fruit) {
-        score += 1;
+        score += weights.fruit;
       }
   
+      // adds appropriate weight to  score if cocktail matches selected mood
       if (cocktail.mood === mood) {
-        score += 1;
+        score += weights.mood;
       }
   
+      // adds appropriate weight to score if cocktail matches selected star sign
       if (cocktail.astrologicalSign === astrologicalSign) {
-        score += 1;
+        score += weights.astrologicalSign;
       }
   
+      // If the score of the cocktail is bigger than the previous max score,
+      // this updates maximum score and recommendeds cocktail
       if (score > maxScore) {
         maxScore = score;
         recommendedCocktail = cocktail;
@@ -33,4 +42,3 @@ export const recommendCocktail = (fruit, mood, astrologicalSign) => {
   
     return recommendedCocktail;
   };
-  
