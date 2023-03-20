46b068ba66d63271c553a00a0f07c01433400de7
harryg <harry@gmail.com>
2023-03-20 22:15:55 +0000

    Initial redesign to default react bootstrap navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -5,39 +5,39 @@ import { NavLink } from 'react-router-dom';
 function Navbar() {
   return (
     <div className="navigation">
-    <ul className="nav nav-tabs">
-      <li className="nav-item nav1">
-        <NavLink
-          to="/"
-          end
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-        Home
-        </NavLink>
-      </li>
-      <li className="nav-item nav2">
-        <NavLink
-          to="directory"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Directory
-        </NavLink>
-      </li>
-      <li className="nav-item nav3">
-        <NavLink
-          to="locator"
-          className={({ isActive }) =>
-            isActive ? 'nav-link active' : 'nav-link'
-          }
-        >
-          Locator
-        </NavLink>
-      </li>
-    </ul>
+      <ul className="nav nav-tabs">
+        <li className="nav-item nav1">
+          <NavLink
+            to="/"
+            end
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Home
+          </NavLink>
+        </li>
+        <li className="nav-item nav2">
+          <NavLink
+            to="directory"
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Directory
+          </NavLink>
+        </li>
+        <li className="nav-item nav3">
+          <NavLink
+            to="locator"
+            className={({ isActive }) =>
+              isActive ? 'nav-link active' : 'nav-link'
+            }
+          >
+            Locator
+          </NavLink>
+        </li>
+      </ul>
     </div>
   );
 }
